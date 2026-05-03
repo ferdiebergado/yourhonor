781184a77e342ecce6afa905fe67e04cf743eef0
ferdie bergado <ferdiebergado@gmail.com>
2026-05-03 10:32:10 +0800

    refactor: Use toast to display errors

--- src/lib/query-client.ts
+++ src/lib/query-client.ts
@@ -1,14 +1,15 @@
 import { MutationCache, QueryCache, QueryClient } from '@tanstack/react-query';
+import { toast } from 'sonner';
 
 export const queryClient = new QueryClient({
   queryCache: new QueryCache({
     onError: (error, query) => {
-      if (query.state.data !== undefined) console.error(error.message);
+      if (query.state.data !== undefined) toast.error(error.message);
     },
   }),
   mutationCache: new MutationCache({
     onError: error => {
-      console.error(error.message || 'Operation failed');
+      toast.error(error.message || 'Operation failed');
     },
   }),
 });
