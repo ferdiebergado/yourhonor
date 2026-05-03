680aeca5278e13afaa1e1404bbfd9036f7dc6b05
ferdie bergado <ferdiebergado@gmail.com>
2026-05-03 20:44:17 +0800

    refactor: Create page header

--- src/app/dashboard.tsx
+++ src/app/dashboard.tsx
@@ -10,18 +10,26 @@ import {
 
 export default function Dashboard() {
   return (
-    <Card className="w-full">
-      <CardHeader>
-        <CardTitle className="font-heading p-3 text-2xl font-semibold">Dashboard</CardTitle>
-        <CardDescription>Card Description</CardDescription>
-        <CardAction>Card Action</CardAction>
-      </CardHeader>
-      <CardContent>
-        <p>Card body</p>
-      </CardContent>
-      <CardFooter>
-        <p>Card Footer</p>
-      </CardFooter>
-    </Card>
+    <>
+      <div className="flex items-center justify-between gap-5 px-7 py-5">
+        <div>
+          <h1 className="font-heading text-2xl font-semibold">Dashboard</h1>
+          <p className="text-muted-foreground">Overview of activities</p>
+        </div>
+      </div>
+      <Card className="w-full">
+        <CardHeader>
+          <CardTitle className="font-heading text-2xl font-semibold">Welcome idol!</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+          <CardAction>Card Action</CardAction>
+        </CardHeader>
+        <CardContent>
+          <p>Card body</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+    </>
   );
 }
