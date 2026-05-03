32d2ef8e3365d89ef2442c3e4cac7cf32d69aa06
ferdie bergado <ferdiebergado@gmail.com>
2026-05-03 10:26:14 +0800

    refactor: Add create focal http handler

---
+++ netlify/functions/create-focal.ts
@@ -0,0 +1,34 @@
+import { getDb } from '@backend/db';
+import { createFocal } from '@backend/focal/repo';
+import { checkMethod, parseJson } from '@backend/http';
+import { respondWithError } from '@backend/http/errors';
+import { getSession } from '@backend/session';
+import { FocalFormSchema, type CreateFocal, type Focal } from '@shared/schemas/focal';
+import type { ApiResponse } from '@shared/types';
+
+export default async (req: Request) => {
+  try {
+    checkMethod(req, ['POST']);
+
+    const data = await parseJson(req, FocalFormSchema);
+
+    const { userId } = await getSession(req);
+
+    const focal: CreateFocal = {
+      ...data,
+      createdBy: userId,
+      updatedBy: userId,
+    };
+
+    const db = await getDb();
+    await createFocal(db, focal);
+
+    const payload: ApiResponse<Focal> = {
+      success: true,
+    };
+
+    return Response.json(payload, { status: 201 });
+  } catch (error) {
+    return respondWithError(error);
+  }
+};
