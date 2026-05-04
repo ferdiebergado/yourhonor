d5cf88f5eefe6290fc7168f0194570332370f41a
ferdie bergado <ferdiebergado@gmail.com>
2026-05-04 09:20:28 +0800

    refactor: Adjust skeleton height

--- src/components/skeleton-card.tsx
+++ src/components/skeleton-card.tsx
@@ -3,13 +3,13 @@ import { Skeleton } from '@/components/ui/skeleton';
 
 export default function SkeletonCard() {
   return (
-    <Card className="w-full max-w-xs">
+    <Card className="w-full">
       <CardHeader>
-        <Skeleton className="h-4 w-2/3" />
+        <Skeleton className="h-8 w-2/3" />
         <Skeleton className="h-4 w-1/2" />
       </CardHeader>
       <CardContent>
-        <Skeleton className="aspect-video w-full" />
+        <Skeleton className="h-28 w-full" />
       </CardContent>
     </Card>
   );
