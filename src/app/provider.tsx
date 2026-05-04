a41a0f39970e49c86b8b22d093fa53832b7bda02
ferdie bergado <ferdiebergado@gmail.com>
2026-05-04 08:16:48 +0800

    refacfor: Move toaster to the provider component

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,24 +1,24 @@
-import ThemeProvider from '@/features/theme/theme-provider';
-import { getCSPNonce } from '@/lib/csp';
-import { queryClient } from '@/lib/query-client';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { type ReactNode } from 'react';
 import { BrowserRouter } from 'react-router';
+
+import { Toaster } from '@/components/ui/sonner';
+import ThemeProvider from '@/features/theme/theme-provider';
+import { queryClient } from '@/lib/query-client';
 
 type ProviderProps = {
   children: ReactNode;
 };
 
 export default function Provider({ children }: ProviderProps) {
-  const nonce = getCSPNonce();
-
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider>
         <BrowserRouter>{children}</BrowserRouter>
+        <Toaster position="top-right" richColors />
       </ThemeProvider>
-      <ReactQueryDevtools styleNonce={nonce} />
+      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 }
