069ce97eda0595ad6de1601ec402dc091596660a
ferdie bergado <ferdiebergado@gmail.com>
2026-05-04 15:02:08 +0800

    refactor: Add tagline

--- src/features/auth/components/signin-page.tsx
+++ src/features/auth/components/signin-page.tsx
@@ -4,11 +4,15 @@ import SigninButton from './signin-button';
 
 export default function SigninPage() {
   return (
-    <Card className="px-6 py-10">
+    <Card className="w-full px-6 py-10 md:max-w-xl">
       <CardContent className="flex flex-col items-center gap-10">
-        <h1 className="text-center text-3xl font-semibold text-balance">
+        <h1 className="font-heading text-center text-3xl font-semibold text-balance">
           Welcome to {config.appTitle}
         </h1>
+        <h2 className="text-center text-lg text-balance">
+          Say goodbye to headaches when preparing payments for honoraria. YourHonor is here to
+          streamline these tasks.
+        </h2>
         <SigninButton />
       </CardContent>
     </Card>
