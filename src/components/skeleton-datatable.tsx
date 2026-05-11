e0a24a6c409b37af7cf216f853dad7749c3e2f35
ferdie bergado <ferdiebergado@gmail.com>
2026-05-11 23:47:10 +0800

    refactor: Add skeletons for pagination controls

--- src/components/skeleton-datatable.tsx
+++ src/components/skeleton-datatable.tsx
@@ -5,19 +5,32 @@ export default function SkeletonDatatable() {
     <>
       <div className="flex items-center justify-between py-4">
         <div>
-          <Skeleton className="h-9 w-[384px]" />
+          <Skeleton className="h-9 w-95.75" />
         </div>
         <div>
-          <Skeleton className="h-9 w-[83.56px]" />
+          <Skeleton className="h-9 w-47" />
         </div>
       </div>
       <div>
         <div>
           <Skeleton className="h-28.5 w-full" />
         </div>
-        <div className="flex items-center justify-end space-x-2 py-4">
-          <Skeleton className="h-8 w-20.5" />
-          <Skeleton className="h-8 w-14.25" />
+        <div className="flex items-center justify-between space-x-2 py-4">
+          <div>
+            <Skeleton className="h-8 w-28" />
+          </div>
+          <div className="flex gap-7">
+            <div className="flex gap-2">
+              <Skeleton className="h-8 w-20" />
+              <Skeleton className="h-8 w-24" />
+            </div>
+            <div className="flex gap-2">
+              <Skeleton className="size-8" />
+              <Skeleton className="size-8" />
+              <Skeleton className="size-8" />
+              <Skeleton className="size-8" />
+            </div>
+          </div>
         </div>
       </div>
     </>
