1c1f136cc0f3de2d5a6c1504cb5241c19ab70c64
ferdie bergado <ferdiebergado@gmail.com>
2026-05-16 17:03:28 +0800

    refactor: Add legal links

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,13 @@
+import PrivacyLink from './PrivacyLink';
+import TermsLink from './TermsLink';
+
 export default function Footer() {
   return (
-    <p className="text-muted-foreground bg-secondary p-5 text-center text-sm">
-      &copy; 2026 to present by ferdie bergado.
-    </p>
+    <div className="text-muted-foreground bg-secondary space-y-5 p-5 text-center text-sm">
+      <p>
+        <TermsLink /> | <PrivacyLink />
+      </p>
+      <p>&copy; 2026 to present by ferdie bergado.</p>
+    </div>
   );
 }
