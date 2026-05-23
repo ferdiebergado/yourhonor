6fe67ec6e78246ad2ee341f61fa234b027ed067c
ferdie bergado <ferdiebergado@gmail.com>
2026-05-23 18:26:58 +0800

    refactor: Change text color to text-destructive

--- src/components/splash-screen.tsx
+++ src/components/splash-screen.tsx
@@ -3,7 +3,7 @@ import config from '@/config';
 export default function SplashScreen() {
   return (
     <section className="bg-background fixed inset-0 z-50 flex items-center justify-center">
-      <h1 className="font-heading animate-pulse text-center text-4xl font-bold text-indigo-600 text-shadow-lg dark:text-indigo-400">
+      <h1 className="font-heading text-destructive animate-pulse text-center text-4xl font-bold text-shadow-lg">
         {config.appTitle}
       </h1>
     </section>
