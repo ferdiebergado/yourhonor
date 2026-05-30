c33286d495f3eb64c074ab98ffa72702f4fdfc4e
ferdie bergado <ferdiebergado@gmail.com>
2026-05-30 14:46:04 +0800

    refactor: Specify return type of signout function

--- src/features/auth/api.ts
+++ src/features/auth/api.ts
@@ -3,8 +3,8 @@ import { api } from '@/lib/http-client';
 import type { Profile } from '@shared/schemas/user';
 
 export const signin = async (code: string): Promise<Profile | null> =>
-  await api.post<Profile>(paths.signin, { code });
+  await api.post(paths.signin, { code });
 
-export const signout = async () => await api.post(paths.signout);
+export const signout = async (): Promise<void | null> => await api.post(paths.signout);
 
 export const fetchMe = async (): Promise<Profile | null> => await api.get<Profile>(paths.me);
