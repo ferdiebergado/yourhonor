70c2634755758dbede508b122d29539a446b2851
ferdie bergado <ferdiebergado@gmail.com>
2026-06-12 13:12:56 +0800

    refactor: Remove type parameters from api calls

--- src/features/auth/api.ts
+++ src/features/auth/api.ts
@@ -5,6 +5,6 @@ import type { Profile } from '@shared/schemas/user';
 export const signin = async (code: string): Promise<Profile | null> =>
   await api.post(paths.signin, { code });
 
-export const signout = async (): Promise<void | null> => await api.post(paths.signout);
+export const signout = async (): Promise<undefined | null> => await api.post(paths.signout);
 
-export const fetchMe = async (): Promise<Profile | null> => await api.get<Profile>(paths.me);
+export const fetchMe = async (): Promise<Profile | null> => await api.get(paths.me);
