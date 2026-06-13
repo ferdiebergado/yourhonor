39015af19c4c765a29f1002217cdec10213497fc
ferdie bergado <ferdiebergado@gmail.com>
2026-06-13 14:56:16 +0800

    refactor: Extract schema parsing into a function

--- src/server/http/index.ts
+++ src/server/http/index.ts
@@ -1,37 +1,47 @@
-import { fromZodError } from '@server/errors';
 import * as z from 'zod';
+
+import { BadRequestError, fromZodError } from '@server/errors';
 
 export type HttpMethod = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE' | 'HEAD' | 'OPTIONS';
 
-export function checkMethod(req: Request, allowedMethods: HttpMethod[]) {
+export function checkMethod(request: Request, allowedMethods: HttpMethod[]) {
   const allowed = new Set<HttpMethod>(allowedMethods);
 
-  if (!allowed.has(req.method as HttpMethod))
+  if (!allowed.has(request.method as HttpMethod))
     return new Response(undefined, { status: 405, headers: { Allow: [...allowed].join(',') } });
 }
 
-export async function parseJson<T extends z.ZodType>(req: Request, schema: T): Promise<z.infer<T>> {
-  const jsonData = await req.json();
-  const { success, error, data } = schema.safeParse(jsonData);
-  if (!success) throw fromZodError(error);
+export async function parseJson<TSchema extends z.ZodType>(
+  request: Request,
+  schema: TSchema
+): Promise<z.output<TSchema>> {
+  let payload: unknown;
 
-  return data;
+  try {
+    payload = await request.json();
+  } catch {
+    throw new BadRequestError('Invalid json body');
+  }
+
+  return validate(schema, payload);
 }
 
-export function parseSearchParams<T extends z.ZodType>(req: Request, schema: T): z.infer<T> {
-  const url = new URL(req.url);
-  const params = Object.fromEntries(url.searchParams.entries());
-  const { success, error, data } = schema.safeParse(params);
-  if (!success) throw fromZodError(error);
-
-  return data;
+export function parseSearchParams<TSchema extends z.ZodType>(
+  request: Request,
+  schema: TSchema
+): z.output<TSchema> {
+  const url = new URL(request.url);
+  const searchParams = Object.fromEntries(url.searchParams.entries());
+  return validate(schema, searchParams);
 }
 
-export function parseRouteParams<T extends z.ZodType>(
+export const parseRouteParams = <TSchema extends z.ZodType>(
   params: Record<string, string>,
-  schema: T
-): z.infer<T> {
-  const { success, error, data } = schema.safeParse(params);
+  schema: TSchema
+): z.output<TSchema> => validate(schema, params);
+
+function validate<TSchema extends z.ZodType>(schema: TSchema, payload: unknown): z.output<TSchema> {
+  const { success, error, data } = schema.safeParse(payload);
   if (!success) throw fromZodError(error);
 
   return data;
