3f944788ebf60712ad05552ff636a432baad3f8b
ferdie bergado <ferdiebergado@gmail.com>
2026-06-13 15:44:03 +0800

    refactor: Add type constraint to form parameter of setFormErrors

--- src/client/lib/utils.ts
+++ src/client/lib/utils.ts
@@ -6,9 +6,6 @@ import { ApiError } from './errors';
 
 export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
 
-export const randomBase64 = (length: number) =>
-  btoa(String.fromCodePoint(...crypto.getRandomValues(new Uint8Array(length))));
-
 export async function startDownload(res: Response, filename: string): Promise<void> {
   const blob = await res.blob();
 
@@ -23,14 +20,14 @@ export async function startDownload(res: Response, filename: string): Promise<vo
   globalThis.URL.revokeObjectURL(url);
 }
 
-export function setFormErrors(form: UseFormReturn, error: Error): void {
+export function setFormErrors<T extends FieldValues>(form: UseFormReturn<T>, error: Error): void {
   if (!(error instanceof ApiError)) return;
 
   const apiError = error as ApiError;
 
   if (apiError.code === 'VALIDATION_ERROR' && apiError.issues) {
     for (const issue of apiError.issues) {
-      form.setError(issue.path as Path<FieldValues>, {
+      form.setError(issue.path as Path<T>, {
         message: issue.message,
       });
     }
