ccfa32b9f73a783b39381fbb941cce7682ce9032
ferdie bergado <ferdiebergado@gmail.com>
2026-06-20 07:42:34 +0800

    refactor: Increase main horizontal padding on large screens

--- src/client/components/layout.tsx
+++ src/client/components/layout.tsx
@@ -9,7 +9,7 @@ export default function Layout() {
   return (
     <div className="flex h-dvh flex-col">
       <Header />
-      <main className="mb-2 w-full flex-1 px-3 pt-4 md:mx-auto md:max-w-5xl">
+      <main className="mb-2 w-full flex-1 px-3 pt-4 md:mx-auto md:max-w-5xl md:px-8">
         <Suspense fallback={<SkeletonCard />}>
           <Outlet />
         </Suspense>
