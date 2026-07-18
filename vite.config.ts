cfc78801f19f07012286c445addb7d2d0181d70e
ferdie bergado <ferdiebergado@gmail.com>
2026-07-18 10:34:26 +0800

    chore: Only enable react compiler in production

--- vite.config.ts
+++ vite.config.ts
@@ -1,51 +1,57 @@
 /// <reference types="vitest/config" />
-import babel from '@rolldown/plugin-babel';
-import tailwindcss from '@tailwindcss/vite';
-import react, { reactCompilerPreset } from '@vitejs/plugin-react';
-import { resolve } from 'node:path';
-import { defineConfig, loadEnv } from 'vite';
+import babel from "@rolldown/plugin-babel";
+import tailwindcss from "@tailwindcss/vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import { resolve } from "node:path";
+import { defineConfig, loadEnv } from "vite";
 
-import { CSP_NONCE_PLACEHOLDER } from './src/shared/constants';
+import { CSP_NONCE_PLACEHOLDER } from "./src/shared/constants";
 
 const dirname = import.meta.dirname;
 
-const alias = {
-  '@client': resolve(dirname, './src/client'),
-  '@server': resolve(dirname, './src/server'),
-  '@shared': resolve(dirname, './src/shared'),
-};
-
-const env = loadEnv('testing', process.cwd(), '');
+const env = loadEnv("testing", process.cwd(), "");
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), babel({ presets: [reactCompilerPreset()] }), tailwindcss()],
-  resolve: {
-    alias,
-  },
-  test: {
-    projects: [
-      {
-        test: {
-          name: 'unit',
-          include: ['src/server/**/*.{test,spec}.ts'],
-          alias,
-          environment: 'node',
-          env,
-        },
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === "production";
+
+  return {
+    plugins: [
+      react(),
+      isProduction && babel({ presets: [reactCompilerPreset()] }),
+      tailwindcss(),
+    ].filter(Boolean),
+    resolve: {
+      alias: {
+        "@client": resolve(dirname, "./src/client"),
+        "@server": resolve(dirname, "./src/server"),
+        "@shared": resolve(dirname, "./src/shared"),
       },
-      {
-        test: {
-          name: 'integration',
-          include: ['tests/integration/**/*.{test,spec}.ts'],
-          alias,
-          environment: 'node',
-          env,
+    },
+    test: {
+      projects: [
+        {
+          extends: true,
+          test: {
+            name: "unit",
+            include: ["src/server/**/*.{test,spec}.ts", "src/shared/**/*.{test,spec}.ts"],
+            environment: "node",
+            env,
+          },
         },
-      },
-    ],
-  },
-  html: {
-    cspNonce: CSP_NONCE_PLACEHOLDER,
-  },
+        {
+          extends: true,
+          test: {
+            name: "integration",
+            include: ["tests/integration/**/*.{test,spec}.ts"],
+            environment: "node",
+            env,
+          },
+        },
+      ],
+    },
+    html: {
+      cspNonce: CSP_NONCE_PLACEHOLDER,
+    },
+  };
 });
